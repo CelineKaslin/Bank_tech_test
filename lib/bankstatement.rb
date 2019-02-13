b42dc25c9f3be7c4cc6ce30a95f0a4d098bf369b
celine kaslin <celine.kaslin@gmail.com>
2019-02-13 14:53:44 +0000

    bankstatement deleted commented code

--- lib/bankstatement.rb
+++ lib/bankstatement.rb
@@ -5,12 +5,6 @@ class BankStatement
     @all_transactions = accountflow
   end
 
-  # def print_statement
-  #   puts 'date || credit || debit || balance'
-  #   @all_transactions.flow.reverse_each do |transaction|
-  #     puts "#{transaction.date} || #{transaction.credit_made} || #{transaction.debit_made} || #{transaction.balance}"
-  #   end
-  # end
   def print_statement
     display = "date || credit || debit || balance\n"
     @all_transactions.flow.reverse_each do |transaction|
